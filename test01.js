1e5d799c2c22a5cac367642a1e66fd9d53db3adc
Chiratchaya Charoenying <chiratchaya_char@cmu.ac.th>
2025-02-07 22:28:59 +0700

    initial commit

--- test01.js
+++ test01.js
@@ -5,3 +5,13 @@ function calculate(x, y) {
         return x * y + x / y;
     }
 }
+
+let x = 7
+if (x > 10) {
+    console.log('x is more than 10')
+} else if (x < 5) {
+    console.log('x is less than 5')
+}
+else {
+    console.log('x is between 5 and 10')
+}
